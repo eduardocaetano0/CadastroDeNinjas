68859783c2332e0fac59c1122c1dfd4ef699bc6d
Eduardo Caetano <eduardocaetano110@example.com>
2025-08-21 12:25:15 -0300

    Cria entidade no BD apartir do NinjaModel

---
+++ src/main/java/dev/java10x/cadastrodeninjas/NinjaModel.java
@@ -0,0 +1,54 @@
+package dev.java10x.cadastrodeninjas;
+
+import jakarta.persistence.*;
+
+
+//Entity transforma uma classe em uma entidade do banco de dados
+//JPA = java persistence
+@Entity
+@Table(name = "tb_cadastro")
+public class NinjaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private int idade;
+    private String email;
+    public NinjaModel(){
+
+    }
+    public NinjaModel(String nome, int idade, String email) {
+        this.nome = nome;
+        this.idade = idade;
+        this.email = email;
+    }
+
+    public NinjaModel(Long id) {
+        this.id = id;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
