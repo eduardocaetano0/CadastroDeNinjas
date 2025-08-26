59a2e0c0b80d1bed4ceaf958d81d6807d4c71991
Eduardo Caetano <eduardocaetano110@example.com>
2025-08-26 16:54:41 -0300

    feat: Coloca o lombok na classe para abstração

--- src/main/java/dev/java10x/cadastrodeninjas/Missoes/MissoesModel.java
+++ src/main/java/dev/java10x/cadastrodeninjas/Missoes/MissoesModel.java
@@ -1,42 +1,29 @@
 package dev.java10x.cadastrodeninjas.Missoes;
 
-
 import dev.java10x.cadastrodeninjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class MissoesModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
-    private char dificuldade;
+    private String dificuldade;
 
     //@OneToMany --> uma missão para muitos ninjas
     @OneToMany(mappedBy = "missoes")
     private List<NinjaModel> ninjas;
 
-    public MissoesModel() {
-    }
-    public MissoesModel(char dificuldade, String nome) {
-        this.dificuldade = dificuldade;
-        this.nome = nome;
-    }
 
-    public char getDificuldade() {
-        return dificuldade;
-    }
-    public void setDificuldade(char dificuldade) {
-        this.dificuldade = dificuldade;
-    }
-    public String getNome() {
-        return nome;
-    }
-    public void setNome(String nomeD) {
-        this.nome = nome;
-    }
 }
--- src/main/java/dev/java10x/cadastrodeninjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/cadastrodeninjas/Ninjas/NinjaModel.java
@@ -2,14 +2,17 @@ package dev.java10x.cadastrodeninjas.Ninjas;
 
 import dev.java10x.cadastrodeninjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
-
-import java.util.List;
-
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 //Entity transforma uma classe em uma entidade do banco de dados
 //JPA = java persistence
 @Entity
 @Table(name = "tb_cadastro")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class NinjaModel {
 
     @Id
@@ -21,33 +24,8 @@ public class NinjaModel {
 
     //@ManyToOne --> Um ninja tem uma única missão.
     @ManyToOne
-    @JoinColumn(name = "missoes_id") //Foring Key ou chave estrangeira
+    @JoinColumn(name = "missoes_id") //Foring Key ou chave estrangeira | conecta tabelas
     private MissoesModel missoes;
-    public NinjaModel(){
 
-    }
-    public NinjaModel(String nome, int idade, String email) {
-        this.nome = nome;
-        this.idade = idade;
-        this.email = email;
-    }
 
-    public String getNome() {
-        return nome;
-    }
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-    public int getIdade() {
-        return idade;
-    }
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
