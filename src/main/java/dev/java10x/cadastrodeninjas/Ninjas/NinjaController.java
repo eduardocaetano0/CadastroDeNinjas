790d0f96c2ed5ab344069629afb2264ed5942df4
Eduardo Caetano <eduardocaetano110@example.com>
2025-09-16 16:05:42 -0300

    feat:Rota de FindbyId adicionada, rota de findAll adicionada, Rota de save adicionada

--- src/main/java/dev/java10x/cadastrodeninjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/cadastrodeninjas/Ninjas/NinjaController.java
@@ -2,9 +2,16 @@ package dev.java10x.cadastrodeninjas.Ninjas;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/ninja")
 public class NinjaController {
+    private NinjaService ninjaService;
+
+    public NinjaController(NinjaService ninjaService) {
+        this.ninjaService = ninjaService;
+    }
 
     @GetMapping("/boasVindas")
     public String boasVindas(){
@@ -12,20 +19,20 @@ public class NinjaController {
     }
 
     //add ninja(CREATE)
-    @PostMapping("/criar")
-    public String criaNinja(){
-        return "Ninja criado";
+    @PostMapping("/criar")      //@RequestBody ->
+    public NinjaModel criarNinja(@RequestBody NinjaModel ninja){
+        return ninjaService.criarNinja(ninja);
     }
 
     //mostrar ninjas(READ)
     @GetMapping("/listar")
-    public String mostrarTodos(){
-        return "Todos os ninjas";
+    public List<NinjaModel> listarTodos(){
+        return ninjaService.listarNinja();
     }
     //mostrar ninja por id(READ)
-    @GetMapping("/listarID")
-    public String mostrarTodosID(){
-        return "Todos os ninjas por ID";
+    @GetMapping("/listar/{id}")
+    public NinjaModel listarTodosID(@PathVariable Long id){
+        return ninjaService.alistarNinjaPorId(id);
     }
     //alterar dados ninja(UPDATE)
     @PutMapping("/alterarID")
--- src/main/java/dev/java10x/cadastrodeninjas/Ninjas/NinjaService.java
+++ src/main/java/dev/java10x/cadastrodeninjas/Ninjas/NinjaService.java
@@ -1,4 +1,31 @@
 package dev.java10x.cadastrodeninjas.Ninjas;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class NinjaService {
+
+    private NinjaRepository ninjaRepository;
+    public NinjaService(NinjaRepository ninjaRepository) {
+        this.ninjaRepository = ninjaRepository;
+    }
+
+    //Listar todos os ninjas
+    public List<NinjaModel> listarNinja(){
+        return ninjaRepository.findAll();
+    }
+
+    //listar por id
+    public NinjaModel alistarNinjaPorId(Long id){
+        Optional<NinjaModel>ninjaPorId = ninjaRepository.findById(id);
+        return ninjaPorId.orElse(null);
+    }
+
+   //Criar ninja
+    public NinjaModel criarNinja(NinjaModel  ninja){
+        return ninjaRepository.save(ninja);
+    }
 }
