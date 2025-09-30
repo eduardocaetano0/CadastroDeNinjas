71bc33c7b27ce91ec4940c2d2f7393521b2c185a
Eduardo Caetano <eduardocaetano110@gmail.com>
2025-09-30 13:03:26 -0300

    refactor: Inclui a a annotation ToString nos models

--- src/main/java/dev/java10x/cadastrodeninjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/cadastrodeninjas/Ninjas/NinjaModel.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 //Entity transforma uma classe em uma entidade do banco de dados
 //JPA = java persistence
@@ -13,6 +14,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@ToString(exclude = "missoes")
 public class NinjaModel {
 
 
